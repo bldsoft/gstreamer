27b66de6b0696dfecb756237ff6a0f43827830f0
Anton Martynov <a.martynau@setplex.com>
2024-07-12 17:24:37 +0300

    [STR-931] Add "with device %d" for NVDEC

--- subprojects/gst-plugins-bad/sys/nvcodec/gstnvvp8dec.cpp
+++ subprojects/gst-plugins-bad/sys/nvcodec/gstnvvp8dec.cpp
@@ -229,10 +229,18 @@ gst_nv_vp8_dec_class_init (GstNvVp8DecClass * klass,
   element_class->set_context = GST_DEBUG_FUNCPTR (gst_nv_vp8_dec_set_context);
 
   parent_class = (GTypeClass *) g_type_class_peek_parent (klass);
-  gst_element_class_set_metadata (element_class,
-      "NVDEC VP8 Decoder",
+
+  gchar *long_name;
+  if (cdata->cuda_device_id) {
+    long_name = g_strdup_printf ("NVDEC VP8 Decoder with device %d",
+        cdata->cuda_device_id);    
+  } else {
+    long_name = g_strdup ("NVDEC VP8 Decoder");
+  }
+  gst_element_class_set_metadata (element_class, long_name,
       "Codec/Decoder/Video/Hardware",
       "NVIDIA VP8 video decoder", "Seungha Yang <seungha@centricular.com>");
+  g_free (long_name);
 
   auto pad_templ = gst_pad_template_new ("sink",
       GST_PAD_SINK, GST_PAD_ALWAYS, cdata->sink_caps);
