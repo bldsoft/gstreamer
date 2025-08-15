b63573569376bffdc225e65666105cd015ecc823
Artsiom Viarenik <a.viarenik@setplex.com>
2025-08-15 12:59:25 +0300

    [STR-3855] mpegts: always write data_alignment_indicator if the flag is set
    It was only written for the first packet

--- subprojects/gst-plugins-bad/gst/mpegtsmux/tsmux/tsmuxstream.c
+++ subprojects/gst-plugins-bad/gst/mpegtsmux/tsmux/tsmuxstream.c
@@ -549,6 +549,10 @@ tsmux_stream_initialize_pes_packet (TsMuxStream * stream)
   if (stream->stream_type == TSMUX_ST_PES_METADATA) {
     stream->pi.flags |= TSMUX_PACKET_FLAG_PES_DATA_ALIGNMENT;
   }
+  // FIXME: determine how to handle other types
+  if (stream->is_dvb_sub) {
+    stream->pi.flags |= TSMUX_PACKET_FLAG_PES_DATA_ALIGNMENT;
+  }
 
   return TRUE;
 }
